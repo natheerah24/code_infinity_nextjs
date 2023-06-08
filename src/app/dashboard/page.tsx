7f465ba39934ad63e89849045450ceefadd44ce8
natheerah24 <natheerah24@gmail.com>
2023-06-08 17:04:47 +0200

    first commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 import Container from "@/components/container";
+
 export default function Dashboard() {
     return (
         <Container>
-        <h1 className={"text-white font-bold text-2xl"}>Dashboard</h1>
+            <h1 className={"text-white font-bold text-2xl"}>Dashboard</h1>
         </Container>
     )
 }
